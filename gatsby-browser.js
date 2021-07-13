7e64d7203dd79911deac51ba70bb6266f659c47b
Rodrigo Passos <rodrigo@hellodative.com>
2021-07-13 16:01:34 -0400

    refactor video modal

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,16 @@
 
 import './src/styles/site.scss'
 import 'bootstrap/dist/js/bootstrap.bundle'
+
+const videoModal = document.getElementById('videoModal')
+
+videoModal.addEventListener('show.bs.modal', evnt => {
+  const videoUrl = evnt.relatedTarget.href
+  const iFrame = evnt.target.getElementsByTagName('iframe')[0]
+  iFrame.src = `${videoUrl}?autoplay=1&showinfo=0&modestbranding=1&rel=0&mute=1`
+})
+
+videoModal.addEventListener('hide.bs.modal', evnt => {
+  const iFrame = evnt.target.getElementsByTagName('iframe')[0]
+  iFrame.src = '#'
+})
--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,30 @@ const Layout = ({ children }) => (
     <SiteHeader siteTitle="Title" />
     <main>{children}</main>
     <SiteFooter />
+    <div
+      className="modal modal-video fade"
+      id="videoModal"
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby="videoModal"
+      aria-hidden="true"
+    >
+      <button
+        type="button"
+        className="position-absolute top-0 end-0 m-2 btn-close"
+        data-bs-dismiss="modal"
+        aria-label="Close"
+      />
+      <div className="modal-dialog modal-video--wrapper position-relative h-100 d-flex flex-column justify-content-center align-items-center">
+        <div className="modal-content">
+          <div className="modal-video--container">
+            <div className="ratio ratio-16x9">
+              <iframe src="#" title="YouTube video" allowFullScreen />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </>
 )
 
